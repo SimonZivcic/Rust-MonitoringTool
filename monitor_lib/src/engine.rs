72d30499523d7eda66e4c2d2079dc3f8be2a8f86
SimonZivcic <simon.zivcic@gmail.com>
2026-01-10 19:12:49 +0100

    engine fix

--- monitor_lib/src/engine.rs
+++ monitor_lib/src/engine.rs
@@ -3,11 +3,13 @@ use crate::models::Server;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 pub async fn simulate_server_metrics(server: &Server) -> (i32, f32, f32) {
+
     if server.status == "OFF" || server.status == "/" || server.status == "STOPPING" {
         return (0, 0.0, 0.0);
     }
 
     let mut rng = rand::rng();
+    
 
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -16,13 +18,14 @@ pub async fn simulate_server_metrics(server: &Server) -> (i32, f32, f32) {
 
     let offset = (server.id as f64) * 1337.42;
 
+
     let base_ms = match server.port {
         80 | 443 => 15.0,
         27017    => 5.0,
         _        => 40.0,
     };
     let ms = (base_ms + rng.random_range(-0.5..0.5)) as i32;
--
+
     let (efficiency_multiplier, base_load) = match server.cpu_model.to_lowercase().as_str() {
         m if m.contains("i9") || m.contains("ryzen 9") => (0.4, 15.0), 
         m if m.contains("i7") || m.contains("ryzen 7") => (0.7, 25.0),
@@ -35,12 +38,14 @@ pub async fn simulate_server_metrics(server: &Server) -> (i32, f32, f32) {
     let cpu = (base_load + (cpu_wave * 15.0 * efficiency_multiplier) + cpu_jitter).clamp(1.0, 99.0) as f32;
 
     let ram_offset = (server.id as f64) * 9876.54;
-    let ram_wave = ((now + ram_offset) / 12000.0).sin();
+    let ram_wave = ((now + ram_offset) / 12000.0).sin(); 
     
+
     let server_specific_base_ram = 0.2 + ((server.id % 5) as f32 * 0.1); 
     let ram_percent = (server_specific_base_ram + (ram_wave as f32 * 0.15)).clamp(0.1, 0.9);
     
     let ram = (server.max_ram * ram_percent).clamp(0.1, server.max_ram);
+
 
     let final_ms = if server.port == 0 { -1 } else { ms.max(1) };
 
